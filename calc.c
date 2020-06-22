d536742af6b4ed8dd735cff422ba6c5952733d61
Subedar-Sagar <65436867+Subedar-Sagar@users.noreply.github.com>
2020-06-22 08:26:11 +0530

    division_included

--- calc.c
+++ calc.c
@@ -5,9 +5,12 @@ void main(){
   int a=2,b=4;
   int c;
   int d;
+  float e;
   c=a+b;
   d=a-b;
+  e=a/b;
   printf("Addition of two numbers is %d\n",c);
   printf("Subtraction of two numbers is %d\n",d);
+printf("Division of two numbers is %f\n",e);
 
 }
